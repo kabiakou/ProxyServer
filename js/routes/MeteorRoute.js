abb95c35121381106b34fc9f4bdf2255ec1f8635
kabiakou <kobkiril@gmail.com>
2024-03-11 22:24:46 +0100

    - removed extra line

--- js/routes/MeteorRoute.js
+++ js/routes/MeteorRoute.js
@@ -2,7 +2,6 @@ const express = require('express')
 const router = express.Router()
 const meteorService = require('../services/MeteorService')
 
-
 router.get('/', async (req, res) => {
     try {
         res.json(await meteorService.getMeteorsData())
