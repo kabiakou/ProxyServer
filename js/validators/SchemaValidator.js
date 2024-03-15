27a105b190b62bc81ff73f7211604f0bdbbbee62
kabiakou <kobkiril@gmail.com>
2024-03-15 17:16:35 +0100

    - code refactoring

--- js/validators/SchemaValidator.js
+++ js/validators/SchemaValidator.js
@@ -2,14 +2,14 @@ const validate = (schema, property) => {
     return (req, res, next) => {
         console.log(req[property])
         const { error } = schema.validate(req[property])
-        if (!error) {
-            next()
+        if (error) {
+            const { details } = error;
+            const message = details.map(i => i.message).join(',')
+            res.status(400).json({
+                message: message
+            })
         }
-        const { details } = error;
-        const message = details.map(i => i.message).join(',')
-        res.status(400).json({
-            message: message
-        })
+        next()
     }
 }
 
