6ec80b07fa3e33a90be1f7c12a64b52248a3672a
kabiakou <kobkiril@gmail.com>
2024-03-25 20:55:18 +0100

    - added sentry

--- js/app.ts
+++ js/app.ts
@@ -3,12 +3,35 @@ import nunjucks from 'nunjucks'
 import path from 'path'
 import { meteorRouter } from './routes/MeteorRoute'
 import { roverRouter } from './routes/RoverRoute'
+import { nodeProfilingIntegration } from "@sentry/profiling-node"
+import * as Sentry from '@sentry/node'
 
 const app = express()
 const PORT = process.env.PORT
 
+
+Sentry.init({
+    dsn: "https://06af601437eca5ff82eb90e1879fe1e1@o4506972824010752.ingest.us.sentry.io/4506973207920640",
+    integrations: [],
+  });
+
+// The request handler must be the first middleware on the app
+app.use(Sentry.Handlers.requestHandler());
+
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler());
+
+// All your controllers should live here
+app.get("/", function rootHandler(req, res) {
+    res.end("Hello world!");
+  });
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+app.get("/debug-sentry", function mainHandler(req, res) {
+    throw new Error("My first Sentry error!");
+  });
 
 app.use('/meteors', meteorRouter)
 app.use('/rover', roverRouter)
@@ -22,12 +45,23 @@ nunjucks.configure(['views/'], {
     noCache: true
 })
 
-app.use((error: any, req: Request, res: Response, next: NextFunction) => {
-    const statusCode = error.code || 500
-    res.status(statusCode).json({
-        message: error.message
-    })
-})
+// app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+//     const statusCode = error.code || 500
+//     res.status(statusCode).json({
+//         message: error.message
+//     })
+// })
+
+// The error handler must be registered before any other error middleware and after all controllers
+app.use(Sentry.Handlers.errorHandler());
+
+app.use(function onError(err: any, req: any, res: any, next: any) {
+    // The error id is attached to `res.sentry` to be returned
+    // and optionally displayed to the user for support.
+    console.log(res.sentry)
+    res.statusCode = 500;
+    res.end(res.sentry + "\n");
+  });
 
 app.use((req, res) => {
     res.status(404).json({
